2c5991f8fa2914f347dc0dfd3b19e72b51448d77
githukudenis <githukudenis@gmail.com>
2023-05-08 18:04:44 +0300

    model app state to include units

--- app/src/main/java/com/githukudenis/wetha/AppState.kt
+++ app/src/main/java/com/githukudenis/wetha/AppState.kt
@@ -2,7 +2,9 @@ package com.githukudenis.wetha
 
 import android.location.Location
 import com.githukudenis.feature_weather_info.data.repository.Theme
+import com.githukudenis.feature_weather_info.data.repository.Units
 
 data class AppState(
+    val units: Units? = null,
     val appTheme: Theme = Theme.LIGHT
 )
